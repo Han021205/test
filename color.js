9d8b8088ce2d39882eb7ff8f3930380320cdab1e
han <gksehddn05@gmail.com>
2023-10-03 18:37:40 +0900

    javaScript library

--- color.js
+++ color.js
@@ -1,19 +1,22 @@
 var Body = {
     setColor: function (color) {
-        document.querySelector('body').style.color = color;
+        //document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor: function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        //document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor',color);
     }
 }
 var Links = {
     setColor: function (color) {
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i = i + 1;
-        }
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length){
+        //   alist[i].style.color = color;
+        //   i = i + 1;
+        // }
+        $('a').css('color', color); // 모든 a테그 제어 첫번째 인자는 색상변경을 변경 하겠다는 뜻, 두번째 인자는 바꾸고 싶은 색상
     }
 }
 
